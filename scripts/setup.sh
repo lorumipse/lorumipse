7a682b1cc2f22a83ee6879201eb90cf4450cd26d
Viktor Nagy <nagy.ch.viktor@gmail.com>
2016-01-21 00:11:03 +0100

    allow using different corpus file for setup.sh as an argument

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,8 +3,9 @@
 script_dir=$(dirname $0)
 root_dir=$script_dir/..
 template_dir=build/text_template
+corpus_file=${1:-$root_dir/resource/sg3_nom_acc_sentences_xaa.txt.gz}
 
 rm -r "$template_dir"
 mkdir -p "$template_dir"
-gzcat $root_dir/resource/sg3_nom_acc_sentences_xaa.txt.gz | $script_dir/split_corpus.py "$template_dir/"
+gzcat -f $corpus_file | $script_dir/split_corpus.py "$template_dir/"
 cp $root_dir/resource/init_sentence_template.txt "$template_dir/init.txt"
