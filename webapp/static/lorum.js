b14c25c61df274b9682b7ce595c78c267e0e7335
Viktor Nagy <nagy.ch.viktor@gmail.com>
2024-01-14 13:28:05 +0100

    show donation banner after n clicks on more

--- webapp/static/lorum.js
+++ webapp/static/lorum.js
@@ -1,118 +1,150 @@
-var lorum = (function() {
-    var my = {};
+const SHOW_DONATION_BANNER_AFTER_N_MORE = 3;
+const HIDE_CONTROL_FOR_SECS = 10;
 
-    function formatLorum(loremData) {
-        var text = "";
-        var i;
-        var j;
-        var nextSpace = false;
-        for (i = 0; i < loremData.length; ++i) {
-            var sentence = loremData[i];
-            for (j = 0; j < sentence.length; ++j) {
-                var token = sentence[j];
-                var noSpace = token[3];
-                var spaceLeft = nextSpace && noSpace !== "left";
-                nextSpace = noSpace !== "right";
-                if (spaceLeft) {
-                    text += " ";
-                }
-                text += token[0];
-            }
+const lorum = (function () {
+  const my = {};
+
+  function formatLorum(loremData) {
+    let text = "";
+    let i;
+    let j;
+    let nextSpace = false;
+    for (i = 0; i < loremData.length; ++i) {
+      const sentence = loremData[i];
+      for (j = 0; j < sentence.length; ++j) {
+        const token = sentence[j];
+        const noSpace = token[3];
+        const spaceLeft = nextSpace && noSpace !== "left";
+        nextSpace = noSpace !== "right";
+        if (spaceLeft) {
+          text += " ";
         }
-        return text;
+        text += token[0];
+      }
     }
+    return text;
+  }
 
-    function generate(withInitSentence) {
-        var deferred = $.Deferred();
-        var url = "/generate/";
-        if (withInitSentence) {
-            url += "init/";
-        }
-        $.ajax(url).then(function(data) {
-            var text = formatLorum(data);
-            deferred.resolve(text);
-        });
-        return deferred.promise();
+  function generate(withInitSentence) {
+    const deferred = $.Deferred();
+    let url = "/generate/";
+    if (withInitSentence) {
+      url += "init/";
     }
-
-    my.addGeneratedTextToTarget = function(trg_spec, withInitSentence) {
-        var trg = $(trg_spec);
-        var deferred = $.Deferred();
-        generate(withInitSentence).then(function(text) {
-            trg.append("<p>" + text + "</p>");
-            deferred.resolve();
-        });
-        return deferred.promise();
-    };
-
-    my.highlightLastPara = function(parent_spec) {
-        var para = parent_spec + " p:last-child";
-        setTimeout(function() {
-            $(para).addClass("highlighted");
-        }, 0);
-        window.scrollTo(0, document.body.scrollHeight);
-        setTimeout(function() {
-            $(para).removeClass("highlighted");
-        }, 500);
-    }
-
-    return my;
-})();
-
-var lorumNav = (function() {
-    var my = {};
-    var openSectionId = null;
-    
-    function collapseSection(section) {
-        openSectionId = null;
-        $(".collapsible-header li[data-section='" + section + "']")
-            .removeClass("collapsible-header-open")
-            .addClass("collapsible-header-closed");
-        $(".collapsible-section[data-section='" + section + "']").hide("fast");
-        $(".collapsible-close-button").hide("fast");
-    }
-
-    function openSection(section) {
-        openSectionId = section;
-        $(".collapsible-header li[data-section='" + section + "']")
-            .removeClass("collapsible-header-closed")
-            .addClass("collapsible-header-open");
-        $(".collapsible-section[data-section='" + section + "']").show("fast");
-        $(".collapsible-close-button").show("fast");
-    }
-
-    function bindCollapsible(index, item) {
-        var toggleHandler = function(event) {
-            collapseOpenSection();
-            openSection($(item).attr("data-section"));
-        };
-        $(item).click(toggleHandler);
-        $(item).addClass("collapsible-header-closed");
-    }
-
-    function collapseOpenSection() {
-        if (openSectionId) {
-            collapseSection(openSectionId);
-        }
-        $(".collapsible-close-button").hide();
-    }
-
-    my.init = function() {
-        $(".collapsible-section").hide();
-        $(".collapsible-header li").each(bindCollapsible);
-        $(".collapsible-close-button").click(collapseOpenSection)
-    };
-
-    return my;
-})();
-
-$(function() {
-    lorumNav.init();
-    var target = "#lorumtext";
-    $("#more").click(function() {
-        lorum.addGeneratedTextToTarget(target, false).then(function() {
-            lorum.highlightLastPara(target);
-        });
+    $.ajax(url).then(function (data) {
+      const text = formatLorum(data);
+      deferred.resolve(text);
     });
-    lorum.addGeneratedTextToTarget(target, true);
+    return deferred.promise();
+  }
+
+  my.addGeneratedTextToTarget = function (trg_spec, withInitSentence) {
+    const trg = $(trg_spec);
+    const deferred = $.Deferred();
+    generate(withInitSentence).then(function (text) {
+      trg.append("<p>" + text + "</p>");
+      deferred.resolve();
+    });
+    return deferred.promise();
+  };
+
+  my.highlightLastPara = function (parent_spec) {
+    const para = parent_spec + " p:last-child";
+    setTimeout(function () {
+      $(para).addClass("highlighted");
+    }, 0);
+    window.scrollTo(0, document.body.scrollHeight);
+    setTimeout(function () {
+      $(para).removeClass("highlighted");
+    }, 500);
+  };
+
+  let numMoreClicked = 0;
+
+  my.handleMore = (trg_spec) => {
+    ++numMoreClicked;
+    if ((numMoreClicked + 1) % SHOW_DONATION_BANNER_AFTER_N_MORE == 0) {
+      showDonationBanner(trg_spec);
+    } else {
+      lorum.addGeneratedTextToTarget(trg_spec, false).then(function () {
+        lorum.highlightLastPara(trg_spec);
+      });
+    }
+  };
+
+  function showDonationBanner(trg_spec) {
+    hideControlTemporarily();
+    const clone = $("#donation-banner-master")
+      .clone()
+      .prop("id", "donation-banner-clone");
+    const target = $(trg_spec);
+    target.append(clone);
+    window.scrollTo(0, document.body.scrollHeight);
+  }
+
+  function hideControlTemporarily() {
+    $("#control").hide(0);
+    setTimeout(() => {
+      $("#control").show();
+      window.scrollTo(0, document.body.scrollHeight);
+    }, HIDE_CONTROL_FOR_SECS * 1000);
+  }
+
+  return my;
+})();
+
+var lorumNav = (function () {
+  const my = {};
+  let openSectionId = null;
+
+  function collapseSection(section) {
+    openSectionId = null;
+    $(".collapsible-header li[data-section='" + section + "']")
+      .removeClass("collapsible-header-open")
+      .addClass("collapsible-header-closed");
+    $(".collapsible-section[data-section='" + section + "']").hide("fast");
+    $(".collapsible-close-button").hide("fast");
+  }
+
+  function openSection(section) {
+    openSectionId = section;
+    $(".collapsible-header li[data-section='" + section + "']")
+      .removeClass("collapsible-header-closed")
+      .addClass("collapsible-header-open");
+    $(".collapsible-section[data-section='" + section + "']").show("fast");
+    $(".collapsible-close-button").show("fast");
+  }
+
+  function bindCollapsible(index, item) {
+    const toggleHandler = function (event) {
+      collapseOpenSection();
+      openSection($(item).attr("data-section"));
+    };
+    $(item).click(toggleHandler);
+    $(item).addClass("collapsible-header-closed");
+  }
+
+  function collapseOpenSection() {
+    if (openSectionId) {
+      collapseSection(openSectionId);
+    }
+    $(".collapsible-close-button").hide();
+  }
+
+  my.init = function () {
+    $(".collapsible-section").hide();
+    $(".collapsible-header li").each(bindCollapsible);
+    $(".collapsible-close-button").click(collapseOpenSection);
+  };
+
+  return my;
+})();
+
+$(function () {
+  lorumNav.init();
+  const target = "#lorumtext";
+  $("#more").click(function () {
+    lorum.handleMore(target);
+  });
+  lorum.addGeneratedTextToTarget(target, true);
 });
